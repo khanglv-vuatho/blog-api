dcc1c57706c7fb91d6d7c06009f621304106bf35
khanglv-vuatho <khanglv@vuatho.com>
2024-06-06 12:53:31 +0700

    update whitelist

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,4 +3,10 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-export const WHITELIST_DOMAINS = ['http://localhost:3000', 'http://localhost:3001', 'https://cms.trisielts.online', 'https://trisielts.online']
+export const WHITELIST_DOMAINS = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://cms.trisielts.online',
+  'https://trisielts.online',
+  'https://auto.trisielts.online'
+]
